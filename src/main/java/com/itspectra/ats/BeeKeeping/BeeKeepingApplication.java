a1f0a42ffa2709d31ed6c4036a834dd887176840
petros manyeka <bsc-com-36-19@unima.ac.mw>
2022-01-10 10:41:19 -0800

    i set up the program , and i have created 2 classes

---
+++ src/main/java/com/itspectra/ats/BeeKeeping/BeeKeepingApplication.java
@@ -0,0 +1,13 @@
+package com.itspectra.ats.BeeKeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeKeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeKeepingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/itspectra/ats/BeeKeeping/GradePrice.java
@@ -0,0 +1,7 @@
+package com.itspectra.ats.BeeKeeping;
+
+public class GradePrice {
+    private long id;
+    private String grade;
+    private double price;
+}
